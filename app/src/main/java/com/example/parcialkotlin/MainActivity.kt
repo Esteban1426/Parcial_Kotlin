4510c45948b4f5edf2ffb35c67d42f99355330d8
Esteban Bustamante <esteban.bustamanter@uniagustiniana.edu.co>
2024-04-05 08:32:02 -0500

    Agregando parcial

---
+++ app/src/main/java/com/example/parcialkotlin/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.parcialkotlin
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var mensaje: EditText
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        mensaje = findViewById(R.id.Carrera)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+    fun onClick(view: View?) {
+        val Carrera: String = mensaje.getText().toString().trim { it <= ' ' }
+        val intent: Intent = Intent(this, MainActivity2::class.java)
+        startActivity(intent)
+    }
+}
---
+++ app/src/main/java/com/example/parcialkotlin/MainActivity2.kt
@@ -0,0 +1,34 @@
+package com.example.parcialkotlin
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main2)
+
+        val intent: Intent = getIntent()
+        val datoCarrera: TextView = findViewById<TextView>(R.id.datoCarrera)
+
+        val Carrera = intent.getStringExtra("Carrera:")
+
+        datoCarrera.text = Carrera
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
+
